7dbfb65cf864d9bd2b6fd523a63da34dcd06944c
Robin DEL AGUILA <rdel-agu@bess-f2r4s7.clusters.42paris.fr>
2025-03-07 11:11:29 +0100

    fixing linker for gdt

--- boot/source/gdt.c
+++ boot/source/gdt.c
@@ -12,33 +12,29 @@
 
 #include "include/gdt.h"
 
-
-
-// Declare GDT and pointer in C
+//TODO needs to be a static to be declared by the preprocessor
 struct GDTEntry gdt[3];
 struct GDTPtr gdtp;
 
 // Set an entry in the GDT (index, base address, limit, access byte, granularity)
-void set_gdt_entry(int index, uint32 base, uint32 limit, uint8 access, uint8 granularity) {
+void set_gdt_entry(int index, uint32 base, uint32 limit, uint8 access, uint8 granularity)
+{
     gdt[index].base_low = base & 0xFFFF;
     gdt[index].base_middle = (base >> 16) & 0xFF;
     gdt[index].base_high = (base >> 24) & 0xFF;
-
     gdt[index].limit_low = limit & 0xFFFF;
     gdt[index].granularity = ((limit >> 16) & 0x0F) | (granularity & 0xF0);
-
     gdt[index].access = access;
 }
 
 // Inline assembly to load the GDT
 extern void load_gdt(struct GDTPtr* gdtp);
 
-// // Initialize the GDT (Null, Code, Data)
 void init_gdt()
 {
     uint32 gdt_base = 0x00000800;
     gdtp.limit = (sizeof(struct GDTEntry) * 3) - 1;
-    gdtp.base = gdt_base;  // Set the GDT pointer base to 0x00000800
+    gdtp.base = gdt_base;
 
     set_gdt_entry(0, 0, 0x00000, 0x00, 0x0); // NULL DESCRIPTOR
     set_gdt_entry(1, 0, 0xFFFFF, 0x9A, 0xC); // KERNEL CODE
@@ -46,8 +42,6 @@ void init_gdt()
     set_gdt_entry(3, 0, 0xFFFFF, 0xFA, 0xC); // USER CODE
     set_gdt_entry(4, 0, 0xFFFFF, 0xF2, 0xC); // USER DATA
     set_gdt_entry(5, 0, 0xFFFFF, 0x89, 0x0); // TASK STATE
-
-    // Load the GDT
     load_gdt(&gdtp);
 }
 
--- boot/source/include/gdt.h
+++ boot/source/include/gdt.h
@@ -1,9 +1,6 @@
 #ifndef GDT_H
 # define GDT_H
 # include "kfs.h"
-// Manually define fixed-width types (since we are not using stdint.h)
-typedef unsigned short	uint16_t;
-typedef unsigned int	uint32_t;
 
 /* GDT Entry Structure (8 bytes per entry) */
 struct GDTEntry {
@@ -15,10 +12,9 @@ struct GDTEntry {
     uint8  base_high;  // Last 8 bits of segment base
 } __attribute__((packed));
 
-/* GDT Pointer Structure (6 bytes) */
 struct GDTPtr {
-    uint16 limit;  // Size of the GDT - 1
-    uint32 base;   // Address of the GDT
+    uint16 limit;
+    uint32 base;
 } __attribute__((packed));
 
 // Function to initialize and load the GDT
